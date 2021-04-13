11f923eaef50dde9edfe0158ef7a997a608a0bc0
Fabian Eichfeldt <fabian.eichfeldt@ds.mhie.com>
2021-04-13 10:52:29 +0200

    use 3x3 videos to provoke memory leak

--- examples/browser/test/h264-player.js
+++ examples/browser/test/h264-player.js
@@ -2,17 +2,21 @@ const { pipelines } = window.mediaStreamLibrary
 
 const play = host => {
   // Grab a reference to the video element
-  const mediaElement = document.querySelector('video')
+  const mediaElements = document.querySelectorAll('video')
 
-  // Setup a new pipeline
-  const pipeline = new pipelines.Html5VideoPipeline({
-    ws: { uri: `ws://${host}:8854/` },
-    rtsp: { uri: `rtsp://localhost:8554/test` },
-    mediaElement,
-  })
-  pipeline.ready.then(() => {
-    pipeline.rtsp.play()
-  })
+  let i=0;
+  for(let mediaElement of mediaElements) {
+    // Setup a new pipeline
+    const pipeline = new pipelines.Html5VideoPipeline({
+      ws: { uri: `ws://${host}:${8854 + i}/` },
+      rtsp: { uri: `rtsp://localhost:${8554 + i}/test` },
+      mediaElement,
+    })
+    i++;
+    pipeline.ready.then(() => {
+      pipeline.rtsp.play()
+    })
+  }
 }
 
 play(window.location.hostname)
