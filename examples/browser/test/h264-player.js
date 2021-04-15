0f9ca60f3bc3ea70a243c814f706c2643228deb4
Fabian Eichfeldt <fabian.eichfeldt@ds.mhie.com>
2021-04-15 09:20:06 +0200

    easier change amount of videos

--- examples/browser/test/h264-player.js
+++ examples/browser/test/h264-player.js
@@ -1,7 +1,19 @@
 const { pipelines, components } = window.mediaStreamLibrary
 
 const play = host => {
-  // Grab a reference to the video element
+
+  const grid = document.querySelector(".grid")
+  for(let i=0; i < 25; i++) {
+    const div = document.createElement("div")
+    div.setAttribute("class", "container")
+    const video = document.createElement("video")
+    video.setAttribute("class", "player")
+    div.append(video)
+    video.muted=true;
+    video.autoplay = true;
+    grid.append(div)
+  }
+
   const mediaElements = document.querySelectorAll('video')
   const sinks = []
   for(let mediaElement of mediaElements)
